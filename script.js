53af01eb5b9c63aa768b7d3347e8d3bbc397dce1
Alwaz <alwazkazi3@gmail.com>
2021-09-11 11:13:39 -0700

    experimental javascript

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let nav = document.querySelector("#menu_bar");
+let menu = document.getElementById("menu-bar");
 let navBar = document.querySelector(".navbar");
 
-nav.onClick = () => {
-  nav.classList.toggle("fa-times");
-  navBar.classList.toggle("active");
+menu.onClick = () => {
+  menu.classList.toggle("fa-times");
+  // navBar.classList.toggle("active");
 };
 
-window.onscroll = () => {
-  nav.classList.remove("fa-times");
-  navBar.classList.remove("active");
-};
+// window.onscroll = () => {
+//   nav.classList.remove("fa-times");
+//   navBar.classList.remove("active");
+// };
